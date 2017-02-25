05d387e22f5ed48c9b7771369919164ce594956e
root <root@localhost.localdomain>
2017-02-25 16:13:50 +0800

    添加web app并初步设计数据库

--- MsqBackSystem/settings.py
+++ MsqBackSystem/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'web',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'MsqBackSystem.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'MysqlBack',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'POST': '3306',
     }
 }
 
---
+++ web/models.py
@@ -0,0 +1,42 @@
+# _*_coding=utf-8_*_
+from __future__ import unicode_literals
+from django.db import models
+
+# Create your models here.
+#主机信息表
+class HostInfo(models.Model):
+    ID = models.AutoField(primary_key=True)
+    HostIp=models.GenericIPAddressField(unique=True)
+    HostDescription = models.CharField(max_length=500)
+    DelBit = models.BooleanField(default=False)
+    AddTime=models.DateTimeField(auto_now_add=True)
+#数据库信息表
+class DbInfo(models.Model):
+    ID = models.AutoField()
+    HostId = models.OneToOneField(HostInfo)
+    DbName = models.CharField(max_length=50)
+    TableName = models.CharField(max_length=500)
+    UpTime = models.DateTimeField(auto_now=True)
+#项目列表
+class ProjeckInfo(models.Model):
+    ID = models.AutoField(primary_key=True)
+    ProgeckName = models.CharField(max_length=500)
+    DelBit = models.BooleanField(default=False)
+    AddTime=models.DateTimeField(auto_now_add=True,unique=True)
+#数据库备份文件路径信息表
+class DbFileInfo(models.Model):
+    ID = models.AutoField()
+    HostId = models.OneToOneField(HostInfo)
+    DbPath = models.CharField(max_length=200)
+    TablePath = models.CharField(max_length=200)
+#数据库登录信息表
+class DbUserInfo(models.Model):
+    ID = models.AutoField(primary_key=True)
+    HostId = models.OneToOneField(HostInfo)
+    HostUser = models.CharField(max_length=20)
+    HostPwd = models.CharField(max_length=100)
+#主机项目关联表
+class HostMsg(models.Model):
+    ID = models.AutoField(primary_key=True)
+    HostId = models.OneToOneField(HostInfo)
+    ProjeckId = models.OneToOneField(ProjeckInfo)
