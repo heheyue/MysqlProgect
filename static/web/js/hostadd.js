46352b768fe85049b562ce4a7eb75d37c6b8b6fa
root <root@bogon.(none)>
2017-02-28 14:47:33 +0800

    主机添加页面js检测插件加入

--- static/web/js/hostadd.js
+++ static/web/js/hostadd.js
@@ -1,14 +1,38 @@
 /**
  * Created by root on 17-2-27.
  */
-function CheckIP()
-{  var ip = document.getElementById('HostIp').value;
-   var re=/^(\d+)\.(\d+)\.(\d+)\.(\d+)$/;
-   if(re.test(ip))
-   {
-       if( RegExp.$1<256 && RegExp.$2<256 && RegExp.$3<256 && RegExp.$4<256)
-       return true;
-   }
-   alert("IP输入错误");
-   return false;
-}
+$(document).ready(
+    function() {
+	    $("#HostAdd").validVal({
+	        customValidations:{
+                "CheakIp":function (val) {
+                var ip = val
+                var re=/^(\d+)\.(\d+)\.(\d+)\.(\d+)$/;
+                if(re.test(ip))
+                {
+                    if( RegExp.$1<256 && RegExp.$2<256 && RegExp.$3<256 && RegExp.$4<256)
+                    return true;
+                }
+                alert('IP地址有误')
+                return false;
+                }
+            },
+            form:{
+	            onInvalid:function () {
+                    alert("请补全必填字段")
+                }
+            }
+        });
+	});
+
+// function CheckIP()
+// {  var ip = document.getElementById('HostIp').value;
+//    var re=/^(\d+)\.(\d+)\.(\d+)\.(\d+)$/;
+//    if(re.test(ip))
+//    {
+//        if( RegExp.$1<256 && RegExp.$2<256 && RegExp.$3<256 && RegExp.$4<256)
+//        return true;
+//    }
+//    alert("IP输入错误");
+//    return false;
+// }
